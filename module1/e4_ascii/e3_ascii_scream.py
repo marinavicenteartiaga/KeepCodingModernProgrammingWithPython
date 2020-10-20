bea4ea72454cfa09d36f9ecc19443529babddf47
Marina <marina.vicente.artiaga@gmail.com>
2020-10-20 19:05:38 +0200

    Module1, exercise 4: e4_ascii_scream

---
+++ module1/e4_ascii/e3_ascii_scream.py
@@ -0,0 +1,17 @@
+def my_upper(message):
+    result = ""
+
+    for character in message:
+        character_code = ord(character)
+        if character_code >= 97 and character_code <= 122:
+            capital_code = character_code - 32
+            capital_character = chr(capital_code)
+            result = result + capital_character
+        else:
+            result = result + character
+
+    return result
+
+
+words = input("Say something whispering: ")
+print("I shout to you:", my_upper(words))
